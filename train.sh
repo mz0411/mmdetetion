47417178f1dc4d7a2e43183f0d66068de7c7f8ef
mz0411 <a976466014@gmail.com>
2023-01-26 21:21:10 +0800

    V2

--- train.sh
+++ train.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=0,1 PORT=29510 tools/dist_train.sh configs/swin/aicity2022.py 2 
+CUDA_VISIBLE_DEVICES=0,1 PORT=29510 tools/dist_train.sh configs/swin/aicity2023.py 2 --cfg-options
