1768eb7d47ad97539f959b4e8bbaed225ddf73ef
mz0411 <a976466014@gmail.com>
2023-01-28 07:46:28 +0800

    kaggle test

--- configs/swin/aicity2023.py
+++ configs/swin/aicity2023.py
@@ -27,7 +27,11 @@ model = dict(
         convert_weights=True,
         init_cfg=dict(type='Pretrained', checkpoint=pretrained)),
     # neck=dict(in_channels=[96, 192, 384, 768]),
-    neck=dict(in_channels=[96, 192, 384, 768]),
+    neck=dict(
+        type='FPN',
+        in_channels=[256, 512, 1024, 2048],
+        out_channels=256,
+        num_outs=5),
 
         roi_head=dict(
         bbox_head=[
@@ -35,9 +39,9 @@ model = dict(
                 type='ConvFCBBoxHead',
                 num_shared_convs=4,
                 num_shared_fcs=1,
-                in_channels=192,
-                conv_out_channels=192,
-                fc_out_channels=768,
+                in_channels=256,
+                conv_out_channels=256,
+                fc_out_channels=1024,
                 roi_feat_size=7,
                 num_classes=1,
                 bbox_coder=dict(
@@ -46,7 +50,7 @@ model = dict(
                     target_stds=[0.1, 0.1, 0.2, 0.2]),
                 reg_class_agnostic=False,
                 reg_decoded_bbox=True,
-                norm_cfg=dict(type='SyncBN', requires_grad=True),
+                norm_cfg=dict(type='BN', requires_grad=True),
                 loss_cls=dict(
                     type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0),
                 loss_bbox=dict(type='GIoULoss', loss_weight=10.0)),
@@ -54,9 +58,9 @@ model = dict(
                 type='ConvFCBBoxHead',
                 num_shared_convs=4,
                 num_shared_fcs=1,
-                in_channels=192,
-                conv_out_channels=192,
-                fc_out_channels=768,
+                in_channels=256,
+                conv_out_channels=256,
+                fc_out_channels=1024,
                 roi_feat_size=7,
                 num_classes=1,
                 bbox_coder=dict(
@@ -65,7 +69,7 @@ model = dict(
                     target_stds=[0.05, 0.05, 0.1, 0.1]),
                 reg_class_agnostic=False,
                 reg_decoded_bbox=True,
-                norm_cfg=dict(type='SyncBN', requires_grad=True),
+                norm_cfg=dict(type='BN', requires_grad=True),
                 loss_cls=dict(
                     type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0),
                 loss_bbox=dict(type='GIoULoss', loss_weight=10.0)),
@@ -73,9 +77,9 @@ model = dict(
                 type='ConvFCBBoxHead',
                 num_shared_convs=4,
                 num_shared_fcs=1,
-                in_channels=192,
-                conv_out_channels=192,
-                fc_out_channels=768,
+                in_channels=256,
+                conv_out_channels=256,
+                fc_out_channels=1024,
                 roi_feat_size=7,
                 num_classes=1,
                 bbox_coder=dict(
@@ -84,7 +88,7 @@ model = dict(
                     target_stds=[0.033, 0.033, 0.067, 0.067]),
                 reg_class_agnostic=False,
                 reg_decoded_bbox=True,
-                norm_cfg=dict(type='SyncBN', requires_grad=True),
+                norm_cfg=dict(type='BN', requires_grad=True),
                 loss_cls=dict(
                     type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0),
                 loss_bbox=dict(type='GIoULoss', loss_weight=10.0))
@@ -151,4 +155,4 @@ optimizer = dict(
             'norm': dict(decay_mult=0.)
         }))
 lr_config = dict(warmup_iters=1000, step=[27, 33])
-runner = dict(max_epochs=36)
+runner = dict(type='EpochBasedRunner',max_epochs=36)
