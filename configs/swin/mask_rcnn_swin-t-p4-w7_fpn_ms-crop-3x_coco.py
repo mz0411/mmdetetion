8c2bf477740b1382fa86f046b51aa19771ac2506
mz0411 <a976466014@gmail.com>
2023-02-15 18:00:21 +0800

    3

--- configs/swin/mask_rcnn_swin-t-p4-w7_fpn_ms-crop-3x_coco.py
+++ configs/swin/mask_rcnn_swin-t-p4-w7_fpn_ms-crop-3x_coco.py
@@ -26,8 +26,8 @@ model = dict(
         with_cp=False,
         convert_weights=True,
         init_cfg=dict(type='Pretrained', checkpoint=pretrained)),
-    neck=dict(in_channels=[128, 256, 512, 1024]),
-    # neck=dict(in_channels=[96, 192, 384, 768]),
+    # neck=dict(in_channels=[128, 256, 512, 1024]),
+    neck=dict(in_channels=[96, 192, 384, 768]),
     roi_head=dict(
     bbox_head=[
         dict(
