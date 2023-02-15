661edeefe6998050ae2bf4b6969ee033079dcf32
mz0411 <a976466014@gmail.com>
2023-02-15 16:36:11 +0800

    2

--- mmdet/datasets/pipelines/loading.py
+++ mmdet/datasets/pipelines/loading.py
@@ -299,6 +299,7 @@ class LoadAnnotations:
         Returns:
             numpy.ndarray: The decode bitmap mask of shape (img_h, img_w).
         """
+        print(list,mask_ann['counts'])
 
         if isinstance(mask_ann, list):
             # polygon -- a single object might consist of multiple parts
