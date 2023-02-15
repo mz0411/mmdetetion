9efbc98df620813af29678b5244a4ef4393db2a0
mz0411 <a976466014@gmail.com>
2023-02-15 17:06:47 +0800

    2

--- configs/swin/mask_rcnn_swin-t-p4-w7_fpn_ms-crop-3x_coco.py
+++ configs/swin/mask_rcnn_swin-t-p4-w7_fpn_ms-crop-3x_coco.py
@@ -71,7 +71,8 @@ train_pipeline = [
     dict(type='Normalize', **img_norm_cfg),
     dict(type='Pad', size_divisor=32),
     dict(type='DefaultFormatBundle'),
-    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels', 'gt_masks']),
+    # dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels', 'gt_masks']),
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels']),
 ]
 data = dict(train=dict(pipeline=train_pipeline))
 
