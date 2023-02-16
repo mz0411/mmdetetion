427a3b9569a0166744f8316f210f731de1e0420d
mz0411 <a976466014@gmail.com>
2023-02-16 17:44:38 +0800

    禁用mask

--- train.sh
+++ train.sh
@@ -1,4 +1,4 @@
 
-CUDA_VISIBLE_DEVICES=0,1 PORT=29510 tools/dist_train.sh configs/swin/mask_rcnn_swin-s-p4-w7_fpn_fp16_ms-crop-3x_coco.py  2 
+CUDA_VISIBLE_DEVICES=0,1 PORT=29510 tools/dist_train.sh configs/swin/aicity2023.py  2 
 
 
